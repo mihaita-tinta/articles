2dbe35c97e5e771e036a710cf817afa7e7093bad
Mihaita Tinta <mihaita.tinta@ing.com>
2021-06-08 20:46:59 +0300

    generated tests

---
+++ testing-time/src/test/java/com/mih/testing/cassandra/PersonRepository39Test.java
@@ -0,0 +1,30 @@
+ package com.mih.testing.cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = CassandraConfig.class)
+class PersonRepository39Test {
+
+    @Autowired
+    PersonRepository repository;
+
+    @Test
+    public void testFindAll() {
+        List<Person> junit = repository.findAll();
+        assertEquals(1, junit.size());
+    }
+
+    @Test
+    public void testInsert() {
+        Person person = new Person("id-1", "junit-name", 20);
+        Person saved = repository.save(person);
+        assertNotNull(saved);
+    }
+}
---
+++ testing-time/src/test/java/com/mih/testing/kafka/EmbeddedKafka30Test.java
@@ -0,0 +1,49 @@
+ package com.mih.testing.kafka;
+
+import com.mih.testing.PersonService;
+import com.mih.testing.cassandra.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
+classes = {KafkaAutoConfiguration.class, KafkaConsumer.class, KafkaProducer.class})
+@EmbeddedKafka
+class EmbeddedKafka30Test {
+
+    @Autowired
+    private KafkaProducer producer;
+
+    @MockBean
+    PersonService service;
+
+    @Value("${test.topic}")
+    private String topic;
+
+    @Test
+    public void test() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        when(service.save(any())).thenAnswer(r -> {
+            latch.countDown();
+            return r.getArgument(0);
+        });
+
+        Person person = new Person("id-123", "test", 20);
+
+        producer.send(topic, person);
+
+        latch.await(10, TimeUnit.SECONDS);
+        verify(service).save(any());
+    }
+}
